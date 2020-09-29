c5ec328b4501882af79006dc87f145b2ec5b5029
Marcin Wolf <marcinwolf@protonmail.com>
2020-09-29 13:18:15 +0100

    Added character walking animation

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -27,7 +27,7 @@ func move_state(delta):
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
-	
+	move_animation(input_vector)
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -35,6 +35,20 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move()
+
+func move_animation(direction):
+	
+	if direction != Vector2.ZERO:
+		if direction.x > 0:
+			$AnimationPlayer.play("WALK_RIGHT")
+		elif direction.x < 0:
+			$AnimationPlayer.play("WALK_LEFT")
+		elif direction.y > 0:
+			$AnimationPlayer.play("WALK_DOWN")
+		elif direction.y < 0:
+			$AnimationPlayer.play("WALK_UP")
+	else:
+		$AnimationPlayer.play("IDLE")
 
 func move():
 	velocity = move_and_slide(velocity)
