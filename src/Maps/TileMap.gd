9f31d9d2442f3fc9a448fc0395f4da2d83862e0a
Marcin Wolf <marcinwolf@protonmail.com>
2020-10-02 15:17:31 +0100

    Refactored TileMap.gd: Added _change_tile()

--- src/Maps/TileMap.gd
+++ src/Maps/TileMap.gd
@@ -3,10 +3,25 @@ extends TileMap
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		var mousePos = self.get_local_mouse_position()
-		var loc = world_to_map(mousePos)
-		var cell = get_cellv(loc)
-		
-		if cell == 0:
-			set_cellv(loc, 12)
-		elif cell != -1:
-			print("Clicked on cell ", self.tile_set.tile_get_name(cell))
+		var cell_pos = world_to_map(mousePos)
+		var cell = get_cellv(cell_pos)
+		_change_tile(cell, cell_pos)
+
+func _change_tile(cell, cell_pos):
+	if cell == 0:
+		set_cellv(cell_pos, 12)
+		print("Changed cell ", cell_pos)
+	elif cell != -1:
+		print("Clicked on cell ", self.tile_set.tile_get_name(cell))
+
+func _check_player_range():
+	pass
+# Changed cell (0, 0)
+# Changed cell (1, 0)
+# Changed cell (1, 1)
+# Changed cell (0, 1)
+# Changed cell (-1, 1)
+# Changed cell (-1, 0)
+# Changed cell (-1, -1)
+# Changed cell (0, -1)
+# Changed cell (1, -1)
