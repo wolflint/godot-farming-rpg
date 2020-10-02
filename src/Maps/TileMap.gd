96329b97088b77a8cf756c6b64d6c7bc5225cc11
Marcin Wolf <marcinwolf@protonmail.com>
2020-10-02 12:15:58 +0100

    Fixed offset when placing new tiles

--- src/Maps/TileMap.gd
+++ src/Maps/TileMap.gd
@@ -14,11 +14,23 @@ extends TileMap
 #		else:
 #			set_cellv(tilepos, 1)
 
-func _unhandled_input(event):
-	if event.is_action_released("left_mouse_click"):
-		var tilePos = world_to_map(event.global_position)
-		var cell = get_cellv(tilePos)
+#func _unhandled_input(event):
+#	if event.is_action_released("left_mouse_click"):
+#		var tilePos = world_to_map(event.global_position)
+#		var cell = get_cellv(tilePos)
+#		if cell != -1:
+#			print(self.tile_set.tile_get_name(cell))
+#		else:
+#			set_cellv(tilePos, 1)
+
+#onready var player = self.get_parent().get_node("Player")
+
+func _process(delta):
+	var mousePos = self.get_local_mouse_position()
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		var loc = world_to_map(mousePos)
+		var cell = get_cellv(loc)
 		if cell != -1:
-			print(self.tile_set.tile_get_name(cell))
+			print("Clicked on cell ", self.tile_set.tile_get_name(cell))
 		else:
-			set_cellv(tilePos, 1)
+			set_cellv(loc, 1)
