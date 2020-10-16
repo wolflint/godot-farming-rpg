2ec9b116c511e09d902b8f6d01dc70b97079744e
Marcin Wolf <wolflint@protonmail.com>
2020-10-16 18:38:58 +0100

    WIP: Implementing tool hit location indicator.

--- src/Maps/TileMap.gd
+++ src/Maps/TileMap.gd
@@ -1,40 +1,19 @@
 extends TileMap
 
-var cursor_arrow = load("res://Rand/cursor_arrow.png")
-var cursor_hoe = load("res://Rand/cursor_hoe.png")
-
-func _unhandled_input(event):
-	var mousePos = self.get_local_mouse_position()
-	var mouseTilePos = world_to_map(mousePos)
-	
-	if event is InputEventMouseMotion:
-		if _mouse_in_player_range(mouseTilePos):
-			_change_cursor(cursor_hoe)
-		else:
-			_change_cursor(cursor_arrow)
-	
-	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		
-		var cell = get_cellv(mouseTilePos)
-		
-		if _mouse_in_player_range(mouseTilePos):
-			_change_tile(cell, mouseTilePos)
-
-func _change_cursor(cursor):
-	Input.set_custom_mouse_cursor(cursor)
-
-func _change_tile(cell, mouseTilePos):
+func change_tile(cell, clicked_tile):
 	# Check if the clicked tile is a dirt tile
 	if cell == 0:
 		# Set tile to a tilled tile
-		set_cellv(mouseTilePos, 12)
-		print("Changed cell ", mouseTilePos)
+		set_cellv(clicked_tile, 12)
+		print("Changed cell ", clicked_tile)
 	elif cell != -1:
 		print("Clicked on cell ", self.tile_set.tile_get_name(cell))
 
-func _mouse_in_player_range(mouseTilePos):
-	var playerTile = world_to_map($Player.position)
+func get_player_tilemap_position(player):
+	return world_to_map(player.position)
+
+func check_players_click_range(player_tilepos, clicked_tile):
 	# Check if the clicked tile is within the player's 9 tile reach radius
-	if mouseTilePos.x in range(playerTile.x - 1, playerTile.x + 2) and mouseTilePos.y in range(playerTile.y - 1, playerTile.y + 2):
+	if clicked_tile.x in range(player_tilepos.x - 1, player_tilepos.x + 2) and clicked_tile.y in range(player_tilepos.y - 1, player_tilepos.y + 2):
 		return true
 	return false
--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,9 +1,16 @@
 extends KinematicBody2D
 
+# Cursor Sprites
+var cursorArrow = load("res://Rand/cursor_arrow.png")
+var cursorHoe = load("res://Rand/cursor_hoe.png")
+
 # Physics
 export var ACCELERATION = 500
 export var MAX_SPEED = 80
 export var FRICTION = 500
+
+# Input
+var last_input = Vector2.ZERO
 
 # Animation
 onready var animationTree = $AnimationTree
@@ -15,10 +22,19 @@ enum {
 	MOVE,
 }
 
+# Signals
+signal entered_level(player)
+
+# Get the current level.
+onready var current_level = get_parent()
+onready var toolHitLocation = $ToolHitLocationSprite
 var state = MOVE
 var velocity = Vector2.ZERO
 
 func _ready():
+#	connect("entered_level", current_level, "on_player_entered_level")
+#	if current_level != null:
+#		emit_signal("entered_level", self)
 	animationTree.active = true
 
 func _physics_process(delta):
@@ -27,6 +43,34 @@ func _physics_process(delta):
 			pass
 		MOVE:
 			move_state(delta)
+
+func _unhandled_input(event):
+	var mouse_hover_pos = current_level.get_local_mouse_position()
+	var hovered_tile = current_level.world_to_map(mouse_hover_pos)
+	var player_tilepos = current_level.get_player_tilemap_position(self)
+	
+	# Toggle between Hoe/Arrow cursor when player hovers in/out of his range
+	
+	if event is InputEventMouseMotion:
+		if current_level.check_players_click_range(player_tilepos, hovered_tile):
+			change_cursor(cursorHoe)
+			toolHitLocation.global_position = current_level.map_to_world(hovered_tile, true)
+			print("Mouse local position: " + str(get_local_mouse_position()))
+			print("ToolHitLocation global position: " + str(toolHitLocation.global_position))
+			toolHitLocation.visible = true
+		else:
+			change_cursor(cursorArrow)
+			toolHitLocation.visible = false
+	
+	# If mouse is clicked within the player's range, till the soil
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		var cell = current_level.get_cellv(hovered_tile)
+		if current_level.check_players_click_range(player_tilepos, hovered_tile):
+			current_level.change_tile(cell, hovered_tile)
+
+
+func change_cursor(cursor):
+	Input.set_custom_mouse_cursor(cursor)
 
 func move_state(delta):
 	# Set input vector
@@ -40,10 +84,17 @@ func move_state(delta):
 		animationTree.set("parameters/IDLE/blend_position", input_vector)
 		animationTree.set("parameters/WALK/blend_position", input_vector)
 		animationState.travel("WALK")
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+			
 	else:
 		animationState.travel("IDLE")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		var player_tilepos = current_level.get_player_tilemap_position(self)
+		var tool_hit_location_tilepos = player_tilepos + input_vector
+		toolHitLocation.position = current_level.map_to_world(tool_hit_location_tilepos)
+		toolHitLocation.visible = true
+	last_input = input_vector
 	move()
 
 #func set_idle_state(delta):
