da4dacad42f4706ff00c8b7111405058c86a4b85
Marcin Wolf <wolflint@protonmail.com>
2020-10-21 13:57:51 +0100

    Always change the cursor to the arrow in the inventory

--- src/UI/UserInterface.gd
+++ src/UI/UserInterface.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 func _input(event):
 	if event.is_action_pressed("inventory"):
+		Input.set_custom_mouse_cursor(load("res://Rand/cursor_arrow.png"))
 		$Inventory.visible = !$Inventory.visible
 		$Inventory.initialise_inventory()
 
