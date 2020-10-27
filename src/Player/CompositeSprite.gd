8356c7ac5d3779cc0888b51d513a1ead405cc7dc
Marcin Wolf <wolflint@protonmail.com>
2020-10-27 15:48:18 +0000

    Working on player sprites and CompositeSprite

--- src/Player/CompositeSprite.gd
+++ src/Player/CompositeSprite.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
-onready var Body = $SkinSprite
-onready var Top = $TopSprite
-onready var Bottom = $BottomSprite
+onready var Body = $Body
+onready var Top = $Top
+onready var Bottom = $Bottom
 
-export(Texture) var BodySprite = load("res://Player/skin/male_sprite_model.png")
-export(Texture) var TopSprite = load("res://Player/tops/male_sprite_top.png")
-export(Texture) var BottomSprite = load("res://Player/bottoms/male_sprite_bottoms.png")
+export(Texture) var BodyTexture = load("res://Player/skin/male_sprite_model.png")
+export(Texture) var TopTexture = load("res://Player/tops/male_sprite_top.png")
+export(Texture) var BottomTexture = load("res://Player/bottoms/male_sprite_bottoms.png")
 
 func _ready():
-	Body.texture = BodySprite
-	Top.texture = TopSprite
-	Bottom.texture = BottomSprite
+	Body.texture = BodyTexture
+	Top.texture = TopTexture
+	Bottom.texture = BottomTexture
 	
 	for child in get_children():
 		if child.name != "AnimationPlayer":
-			child.vframes = 4
-			child.hframes = 8
+			child.vframes = 1
+			child.hframes = 26
